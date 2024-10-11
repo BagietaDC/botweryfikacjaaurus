afbb1a155a6c3d235285516410c8574a19315d9f
blxee <84454510+BagietaDC@users.noreply.github.com>
2024-10-11 14:09:48 +0200

    Update mainAPI.py

--- mainAPI.py
+++ mainAPI.py
@@ -9,25 +9,27 @@ import mysql.connector
 @app.get("/api/robloxwhitelist/{robloxid}")
 def index(robloxid: str):
     connection = mysql.connector.connect(
-    host="https://titanaxe.com/",
-    user="www14088_tykocinweryfikacja",
-    password="NLCQtx7NWMClKFmjpWcC",
-    database="www14088_tykocinweryfikacja"
-    )
+        host="54.38.50.59",
+        user="www14088_tykocinweryfikacja",
+        password="NLCQtx7NWMClKFmjpWcC",
+        database="www14088_tykocinweryfikacja")
 
     cursor = connection.cursor()
 
-    cursor.execute(f"SELECT DiscordID, NickDiscord, Zweryfikowany FROM tykocin WHERE RobloxID = '{robloxid}';")
+    cursor.execute(
+        f"SELECT DiscordID, NickDiscord, Verified FROM tykocin WHERE RobloxID = '{robloxid}';"
+    )
 
     results = cursor.fetchall()
 
-
     print(results)
     if results:
-      
-      return {'DiscordID' : results[0][0] ,
-              "DiscordNick" : results[0][1],
-              "Verified" : results[0][2]}
+
+        return {
+            'DiscordID': results[0][0],
+            "DiscordNick": results[0][1],
+            "Verified": results[0][2]
+        }
     else:
         return {"Not found"}
 
@@ -36,4 +38,4 @@ def index(robloxid: str):
 
 
 if __name__ == "__main__":
-  uvicorn.run(app,host="0.0.0.0")
+    uvicorn.run(app, host="0.0.0.0")
